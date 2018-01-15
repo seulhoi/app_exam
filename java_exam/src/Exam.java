2acdff78d3d9becf873b906238f39763832c91dd
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-15 12:33:29 +0900

    변수 첫번째예제

---
+++ java_exam/src/Exam.java
@@ -0,0 +1,70 @@
+    /*
+      작성자:남슬기
+      작성일:2018.01.10
+      작성이유: 자바 출력 연습하기!!
+      여러줄을 주석표시 하고싶을땐
+    */
+public class Exam {
+    public static void main(String[] args) {
+        //System.out.println("남슬기"); //이름을 출력하는 명령
+        //System.out.println("Welcome to Java");
+
+        // 출력- 자가진단
+        System.out.println("Programming! It's fun.");
+        System.out.println("My name is Hong Gill Dong");
+        System.out.println("I am 13 yrears old.");
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
+        System.out.println("My height " + 170);
+        System.out.println("My weight "+ "68.6000000");
+        System.out.println(5 + " Dan");
+        System.out.println(5 +"*"+2 +"="+10);
+        System.out.println("===========================");
+        System.out.println("  item  count   price");
+        System.out.println("  pen     "+20+"      "+100);
+        System.out.println("  note     "+5+"       "+95);
+        System.out.println("eraser   "+110+"       "+97);
+        //형성평가
+        System.out.println("===========================");
+        System.out.println("My name is Hong");
+        System.out.println("My hometown");
+        System.out.println("Flowering mountain");
+        System.out.println("TTTTTTTTTT");
+        System.out.println("TTTTTTTTTT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+
+        int kor = 90;
+        int mat = 80;
+        int eng = 100;
+        int sum = 90+80+100;
+        int evg = (90+80+100)/3;
+
+        System.out.println("kor "+90);
+        System.out.println("mat "+80);
+        System.out.println("eng "+100);
+        System.out.println("sum " + sum);
+        System.out.println("evg " + evg);
+
+
+        System.out.println("==========18/01/15===========");
+
+        int num1;
+        num1=10;
+
+        char a = 'a';
+        int num2 = 20;
+        int num3 = num1+num2;
+        System.out.println(num1+"+"+num2+"="+ num3);
+        System.out.println("문자: "+ a);
+
+        System.out.println("===========================");
+
+
+
+
+    }
+}
+
