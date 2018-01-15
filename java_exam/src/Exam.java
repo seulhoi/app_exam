4cc1f1676984f8b5803d0f54f3d3c2cdc491f982
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-15 14:20:37 +0900

    변수선언 두번째

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -6,59 +6,14 @@
     */
 public class Exam {
     public static void main(String[] args) {
-        //System.out.println("남슬기"); //이름을 출력하는 명령
-        //System.out.println("Welcome to Java");
-
-        // 출력- 자가진단
-        System.out.println("Programming! It's fun.");
-        System.out.println("My name is Hong Gill Dong");
-        System.out.println("I am 13 yrears old.");
-        System.out.println("(@) (@)");
-        System.out.println("(=^.^=)");
-        System.out.println("(-m-m-)");
-        System.out.println("My height " + 170);
-        System.out.println("My weight "+ "68.6000000");
-        System.out.println(5 + " Dan");
-        System.out.println(5 +"*"+2 +"="+10);
-        System.out.println("===========================");
-        System.out.println("  item  count   price");
-        System.out.println("  pen     "+20+"      "+100);
-        System.out.println("  note     "+5+"       "+95);
-        System.out.println("eraser   "+110+"       "+97);
-        //형성평가
-        System.out.println("===========================");
-        System.out.println("My name is Hong");
-        System.out.println("My hometown");
-        System.out.println("Flowering mountain");
-        System.out.println("TTTTTTTTTT");
-        System.out.println("TTTTTTTTTT");
-        System.out.println("    TT");
-        System.out.println("    TT");
-        System.out.println("    TT");
-
-        int kor = 90;
-        int mat = 80;
-        int eng = 100;
-        int sum = 90+80+100;
-        int evg = (90+80+100)/3;
-
-        System.out.println("kor "+90);
-        System.out.println("mat "+80);
-        System.out.println("eng "+100);
-        System.out.println("sum " + sum);
-        System.out.println("evg " + evg);
-
-
-        System.out.println("==========18/01/15===========");
-
         int num1;
         num1=10;
 
-        char a = 'a';
+        char A = 'a';
         int num2 = 20;
         int num3 = num1+num2;
         System.out.println(num1+"+"+num2+"="+ num3);
-        System.out.println("문자: "+ a);
+        System.out.println("문자: "+ A);
 
         System.out.println("===========================");
 
