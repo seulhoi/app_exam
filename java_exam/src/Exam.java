f7419250fff50a32f64cbc1e8253fcf8b2c08873
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-16 14:17:10 +0900

    연산자 두번째

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -6,35 +6,15 @@
     */
 public class Exam {
     public static void main(String[] args) {
-        int num1;
-        num1=10;
 
-        char A = 'a';
-        int num2 = 20;
-        int num3 = num1+num2;
-        System.out.println(num1+"+"+num2+"="+ num3);
-        System.out.println("문자: "+ A);
+         int num1, num2;
+         num1=30;
+         num2=60;
+        boolean result;
 
-        System.out.println("===========================");
+         result = num1 > num2;
 
-         long num11 = 10000000000l;
-         float num22 = 13.3f;
-
-         System.out.println("long 사용: "+ num11);
-         System.out.println("float 사용:" + num22);
-        System.out.println("===========================");
-
-         int n1=7;
-         int n2 =3;
-         int resert = n1+n2;
-         System.out.println("n1:"+ n1 +"   n2:"+n2);
-         System.out.println("덧셈 연산: "+ resert);
-
-         resert = n1-n2;
-         System.out.println("뺄셈 연산: "+ resert);
-         System.out.println("곱하기 연산: "+ n1*n2);
-         System.out.println("나누기 연산: "+ n1/n2);
-         System.out.println("나머지 연산: " + n1%n2);
+         System.out.println("결과는 : "+result);
     }
 }
 
