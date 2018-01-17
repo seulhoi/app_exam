24f5714b0b22c74035154222d997adc1fcc7eb3d
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-17 12:48:33 +0900

    if문과 else if문

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,4 +1,6 @@
-    /*
+import javax.sound.midi.SysexMessage;
+
+/*
       작성자:남슬기
       작성일:2018.01.10
       작성이유: 자바 출력 연습하기!!
@@ -7,14 +9,37 @@
 public class Exam {
     public static void main(String[] args) {
 
-         int num1, num2;
-         num1=30;
-         num2=60;
-        boolean result;
+     int num = 11;
 
-         result = num1 > num2;
+     if(num>0){
+         System.out.println("num은 0 보다 큽니다.");
+     }
+     if((num%2)==0){
+         System.out.println("num은 짝수입니다.");
+     }else{
+         System.out.println("num은 홀수입니다.");
+     }
 
-         System.out.println("결과는 : "+result);
+        System.out.println("=========== else if문 ==============");
+
+     int score =88;
+
+     if(score >= 90) {
+         System.out.println("성적은 수 입니다.");
+     }else if (score >= 80)
+     {
+             System.out.println("성적은 우 입니다.");
+         }
+         else if(score >=70){
+             System.out.println("성적은 미 입니다.");
+         }
+         else if(score >=60){
+             System.out.println("성적은 양 입니다.");
+         }else{
+             System.out.println("성적은 가 입니다.");
+         }
+     }
+
     }
-}
+
 
