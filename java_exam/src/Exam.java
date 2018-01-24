b153800b6d8fd31aa4864f094563d458135f1a41
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-24 13:55:11 +0900

    6과 14의 공배수를 찾는 예제(break문사용)

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,30 +3,20 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-//break문
-        int num = 2;
-
-        while(num < 10){
-               if (num%5==0){
+        //숫자를 저장하는 변수
+        int num=1;
+        //1씩 증가시키면서 6의 배수 이고 14의 배수인지 반복하여 무한루프로 검사
+        while(true){
+             //6의 나머지가 0이고 14의 나머지가 0이어야 if부분이 true
+            if(num%6==0 && num%14==0){
+             //무한 루프 탈출
                 break;
-                 }
-            System.out.println(num);
-            num++;
-          }
-        System.out.println("============================");
-         
-//continue문
-        int num2 = 2;
-
-        while(num2 < 10){
-            if (num2%5==0){
-                num2++;
-                continue;
             }
-            num2++;
-            System.out.println(num2);
-
+             //숫자 1증가
+            num++;
         }
+        //6과 14의 공배수 출력
+        System.out.println(num);
     }
 }
 
