075ff2410f61563eca666f526536563eddf8d503
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-29 14:59:22 +0900

    0129 메소드

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        //숫자를 저장하는 변수
-        int num=1;
-        //1씩 증가시키면서 6의 배수 이고 14의 배수인지 반복하여 무한루프로 검사
-        while(true){
-             //6의 나머지가 0이고 14의 나머지가 0이어야 if부분이 true
-            if(num%6==0 && num%14==0){
-             //무한 루프 탈출
-                break;
-            }
-             //숫자 1증가
-            num++;
-        }
-        //6과 14의 공배수 출력
-        System.out.println(num);
+       System.out.println("프로그램의 시작");
+       //메소드 실행(호출)방법
+       hiEveryone(12);
+       hiEveryone(13);
+       System.out.println("프로그램의 끝");
+    }
+                                //매게변수(int age)
+    public static void hiEveryone(int age){
+        System.out.println("좋은 아침입니다");
+        System.out.println("제 나이는"+ age+"세 입니다");
     }
 }
 
