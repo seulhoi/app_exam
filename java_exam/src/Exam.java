cb05f982cf0866ec53f10dfb05ecf848d49291fc
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-31 10:48:59 +0900

    01/31 반지름길이를 입력하여 넓이를 출력하는 프로그램

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,94 +3,46 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-    //사용자가 입력한 숫자를 저장하는 변수
-       double num1;
-       double num2;
+        //반지름의 길이를 입력받아 넓이를 출력하는 프로그램 01/31
+        //사용자가 입력한 숫자를 저장하는 변수
+        int num1;
+        //사용자로 키보드 입력준비
+        Scanner kb = new Scanner(System.in);
+        // 사용자로부터 숫자 입력
+        System.out.println("반지름을 입력하세요.");
+        num1 = kb.nextInt();
 
-       String operator;
+        System.out.println("원의넓이는 ?");
+        System.out.println(area(num1));
 
-       //사용자 키보드 입력준비
-       Scanner kb = new Scanner(System.in);
-
-       System.out.println("첫번째 숫자입력");
-       num1 = kb.nextDouble(); //숫자를 입력받는 함수
-        //개행문자 제거를 위한 flush 처리
-        kb.nextLine();
-
-        System.out.println("a~e 연산지 선택:");
-        operator = kb.nextLine();
-
-        System.out.println("두번째 숫자입력");
-        num2 = kb.nextDouble();
-
-        switch (operator){
-            case "a":
-                System.out.println("덧셈한 결과는?");
-                System.out.println(adder(num1,num2));
-                break;
-            case "b":
-                System.out.println("곱셈 결과는?");
-                System.out.println(multi(num1,num2));
-                break;
-            case "c":
-                System.out.println("나누기 결과는?");
-                if (num2 == 0){
-                    System.out.println("나눗셈 못함");
-                }else{
-                    System.out.println(divide(num1,num2));
-                }break;
-            case "d":
-                System.out.println("뺄셈한 결과는?");
-                System.out.println(minus(num1,num2));
-                break;
-            case "e":
-                System.out.println("나머지 결과는?");
-                System.out.println(modular(num1,num2));
-                break;
-                default:
-                    System.out.println("연산을 잘못입력하였습니다.");
-        }
     }
-
-    public static double absolute (double num1){
+//01.31 순서도 area()함수 프로그램
+    public static double area(int radian){
+        //반지름*반지름을 저장할 변수
+        double temp;
+        //원주율을 저장할 변수
         double result;
-        if(num1 >=0){
-            result = num1;
-            } else{
-            result = num1 * -1;
-        } return num1;
-    }
 
-
-    public static double adder(double num1, double num2){
-        // 덧셈값을 저장하는 변수 생성
-        double result;
-        //덧셈실행
-        result = num1 + num2;
-        //덧셈한 결과값을 리턴(도출)
+        temp= multi(radian, radian);
+        result = multi(temp, 3.14);
+        //result = radian*radian*3.14
         return result;
     }
+
+//   01.30 순서도 area() 함수 프로그램
+//    public static double area(int radian){
+//        double temp;
+//        double result;
+//
+//        result = radian*radian*3.14;
+//        return result;
+//    }
+
     public static double multi(double num1, double num2){
-        //곱셈값을 저장하는 변수 생성
         double result;
         //곱셈연산 실행
-        result= num1*num2;
-        //곱셈한 결과값을 리턴(도출)
-        return result;
-    }
-    public static double divide(double num1, double num2){
-        double result;
-        result= num1/num2;
-        return result;
-    }
-    public static double minus(double num1, double num2){
-        double result;
-        result= num1-num2;
-        return result;
-    }
-    public static double modular(double num1, double num2){
-        double result;
-        result= num1%num2;
+        result = num1 * num2;
+        //곱셈한 결과 값을 리턴(도출)
         return result;
     }
 }
