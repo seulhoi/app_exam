8be27f32c1c7074776e8114edcb9910a96746f29
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-01-31 12:50:26 +0900

    0131 함수예제2 (1부터 입력받은값의 합 구하는 프로그램)

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,46 +3,43 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        //반지름의 길이를 입력받아 넓이를 출력하는 프로그램 01/31
+    /* 1부터 전달받은 수까지 합을 출력하는 하수를 작성하고
+     1000이하의 자연수를 입력받아 작성한 함수로 전달하여 출력하는 프로그램 */
+
         //사용자가 입력한 숫자를 저장하는 변수
         int num1;
-        //사용자로 키보드 입력준비
+        //사용자가 키보드 입력준비
         Scanner kb = new Scanner(System.in);
-        // 사용자로부터 숫자 입력
-        System.out.println("반지름을 입력하세요.");
+        //사용자로부터 숫자 입력받음
+        System.out.println("숫자를 입력하세요.");
         num1 = kb.nextInt();
 
-        System.out.println("원의넓이는 ?");
-        System.out.println(area(num1));
+        //type1
+        if(num1 > 1000){
+            System.out.println("입력한 숫자가 1000 보다 큽니다.");
+            return;
+        }else{
+            System.out.println("1에서 입력한 숫자까지의 합은?");
+            System.out.println(addOneToValue(num1));
+        }
 
+       /* //type2
+        if(num1>1000){
+            System.out.println("입력한 숫자가 1000보다 큽니다.");
+            return;
+        }
+        System.out.println("1에서 입력한 숫자까지의 합은?");
+        System.out.println(addOneToValue(num1));
+    */
     }
-//01.31 순서도 area()함수 프로그램
-    public static double area(int radian){
-        //반지름*반지름을 저장할 변수
-        double temp;
-        //원주율을 저장할 변수
-        double result;
+    public static int addOneToValue(int num1){
+        int temp=1;
+        int result = 0;
 
-        temp= multi(radian, radian);
-        result = multi(temp, 3.14);
-        //result = radian*radian*3.14
-        return result;
-    }
-
-//   01.30 순서도 area() 함수 프로그램
-//    public static double area(int radian){
-//        double temp;
-//        double result;
-//
-//        result = radian*radian*3.14;
-//        return result;
-//    }
-
-    public static double multi(double num1, double num2){
-        double result;
-        //곱셈연산 실행
-        result = num1 * num2;
-        //곱셈한 결과 값을 리턴(도출)
+        while(temp <= num1){
+            result = result + temp;
+            temp= temp+1;
+        }
         return result;
     }
 }
