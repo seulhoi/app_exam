4860d352800e694c3f1b61e0484e31ffb448c46b
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-14 11:25:23 +0900

    0213_자바시험

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        Vender myVender = new Vender();
-        Vender myVender2 = new Vender();
+        Scanner kb = new Scanner(System.in);
 
-        //돈 입력
-        myVender.inputMoney();
-        //커피 선택
-        myVender.selectTea();
-        //커피 만들기
-        myVender.makeTea(myVender.tea);
-        //잔돈 계산하기
-        int return_money = myVender.returnMoney(myVender.tea, myVender.user_money);
-        //잔돈 출력하기
-        System.out.println(return_money);
+        System.out.println("변환할 미터값을 입력하시오");
+        double num3 = kb.nextInt();
 
+
+       double InchNum= num3 *39.37;
+       double FeetNum = num3*3.2808;
+       double YardNum = num3*1.0936;
+
+        System.out.println("미터(M)를 인치(Inch)로 변환한 결과: "+InchNum);
+        System.out.println("미터(M)를 인치(Feet)로 변환한 결과: "+FeetNum);
+        System.out.println("미터(M)를 인치(Yard)로 변환한 결과: "+YardNum);
     }
 }
 
--- java_exam/src/Number.java
+++ java_exam/src/Number.java
@@ -1,9 +1,12 @@
 class Number {
     int num;
+
+    //생성자
     Number(){
         num = 10;
         System.out.println("생성자 호출!");
     }
+
     public int getNumber(){
         return num;
     }
