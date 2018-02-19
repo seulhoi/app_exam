c10c9d7c768efec4f01632d66cfcb724a7890a43
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-19 16:31:22 +0900

    연습문제)[배열] 정수10개를 저장하여 차례대로 출력하는 프로그램작성

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        //배열
-        String[] strArray = new String[3];
-        strArray[0] = new String("Java");
-        strArray[1] = new String("Flex");
-        strArray[2] = new String("Ruby");
+        //정수 10개를 저장할 수 있ㄴ쯘 배열 생성
+        int[] arr =new int[10];
 
-        //for(초기값; 비교; 증가)
-        for(int i = 0; i < strArray.length; i++){
-            System.out.println(strArray[i]);
+        //반복하여 입력받기 위한 횟수를 저장하는 변수 생성
+        int num =0;
+
+        //10번 입력받고 정수를 저장하는 반복문
+        while(num<10){
+            //사용자에게 입력받음
+            Scanner kb = new Scanner(System.in);
+            //배열에 사용자에게 받은 값 저장
+            arr[num] = kb.nextInt();
+            //10번 반복해서 반복횟수 증가
+            num++;
+        }
+        //배열의 값을 반복문으로 출력함
+        for(int i =0; i<arr.length; i++){
+            System.out.println(arr[i]);
         }
     }
 }
