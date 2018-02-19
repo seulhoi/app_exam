df2781ea75ce5ebfa8bfd012b02861def09546d7
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-19 17:09:42 +0900

    연습문제)[배열] 5개정수를 입력받은 후 1,3,5 번째 정수의 합을 출력하는 프로그램

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -3,25 +3,19 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        //정수 10개를 저장할 수 있ㄴ쯘 배열 생성
-        int[] arr =new int[10];
+        //5개의 정수를 입력받은 후
+        // 첫번째와 세번째와 다섯번째 정수를 더한 값을 출력하는 프로그램
+        int[] arr = new int[5];
+        Scanner kb = new Scanner (System.in);
 
-        //반복하여 입력받기 위한 횟수를 저장하는 변수 생성
-        int num =0;
+        arr[0] = kb.nextInt();
+        arr[1] = kb.nextInt();
+        arr[2] = kb.nextInt();
+        arr[3] = kb.nextInt();
+        arr[4] = kb.nextInt();
 
-        //10번 입력받고 정수를 저장하는 반복문
-        while(num<10){
-            //사용자에게 입력받음
-            Scanner kb = new Scanner(System.in);
-            //배열에 사용자에게 받은 값 저장
-            arr[num] = kb.nextInt();
-            //10번 반복해서 반복횟수 증가
-            num++;
-        }
-        //배열의 값을 반복문으로 출력함
-        for(int i =0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
+        int result = arr[0] + arr[2] + arr[4];
+        System.out.println(result);
     }
 }
 
