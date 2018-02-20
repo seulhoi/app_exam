66c88a57560a49864ff8953c6e42a6c2ca3e1469
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-20 10:48:22 +0900

    Swing 컴포넌트와 event 핸들링(window창 띄우기)

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,27 +1,46 @@
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+      //frame 객체 생성
+       JFrame frame = new JFrame("title");
+       //frame 크기 설정(화면의 x좌표,y좌표,창 너비,창 높이)
+       frame.setBounds(120,120,400,100);
+       //프레임안에 배치될 버튼들의 배치방식 설정
+        frame.setLayout(new FlowLayout());
 
-        System.out.println("데이터저장");
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
+        //닫기 버튼 동작을 ㅜ이한 리스너 설정 및 등록
+        WindowListener listener = new WindowAdapter() {
+            public void windowClosing(WindowEvent we){
+                System.exit(0);
+            }
+            public void windowOpened(WindowEvent we){
+                System.out.println("프로그램이 수행 되었습니다.");
+            }
+        };
 
-        System.out.println("데이터 읽기");
-        System.out.println(arrayList.get(0));
-        System.out.println(arrayList.get(1));
-        System.out.println(arrayList.get(2));
+        //버튼 객체를 생성
+        JButton btn1 = new JButton("Btn");
+        JButton btn2 = new JButton("Btn");
+        JButton btn3 = new JButton("Btn");
 
-        System.out.println("데이터 삭제");
-        arrayList.remove(0);
-        for(int i =0; i< arrayList.size(); i++){
-            System.out.println(arrayList.get(i));
-        }
+        //버튼을 프레임 안에 추가
+        frame.add(btn1);
+        frame.add(btn2);
+        frame.add(btn3);
+
+        //프레임을 화면에 보여줌
+        frame.setVisible(true);
+
+
+
+
     }
 }
 
