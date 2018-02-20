5f3511c2322484ed5920a04e455f30c8116c6603
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-20 11:55:42 +0900

    프레임안에 배치될 버튼방식_그리드레이아웃

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -12,39 +12,28 @@ public class Exam {
        JFrame frame = new JFrame("title");
        //frame 크기 설정(화면의 x좌표,y좌표,창 너비,창 높이)
        frame.setBounds(120,120,400,100);
-       //프레임안에 배치될 버튼들의 배치방식 설정
-        frame.setLayout(new FlowLayout());
+       //프레임안에 배치될 버튼들의 배치방식 설정 그리드레이아웃 (행,열)
+        frame.setLayout(new GridLayout(3,2));
 
-        //닫기 버튼 동작을 ㅜ이한 리스너 설정 및 등록
-        WindowListener listener = new WindowAdapter() {
-            public void windowClosing(WindowEvent we){
-                System.exit(0);
-            }
-            public void windowOpened(WindowEvent we){
-                System.out.println("프로그램이 수행 되었습니다.");
-            }
-        };
-
-        frame.addWindowListener(listener);
-
-        MouseEventHandler meHandler = new MouseEventHandler();
 
 
         //버튼 객체를 생성
-        JButton btn1 = new JButton("Btn");
-        JButton btn2 = new JButton("Btn");
-        JButton btn3 = new JButton("Btn");
+        JButton btn1 = new JButton("Btn1");
+        JButton btn2 = new JButton("Btn2");
+        JButton btn3 = new JButton("Btn3");
+        JButton btn4 = new JButton("Btn4");
+        JButton btn5 = new JButton("Btn5");
+        JButton btn6 = new JButton("Btn6");
 
         //버튼을 프레임 안에 추가
         frame.add(btn1);
         frame.add(btn2);
         frame.add(btn3);
-
+        frame.add(btn4);
+        frame.add(btn5);
+        frame.add(btn6);
         //프레임을 화면에 보여줌
         frame.setVisible(true);
-
-
-
 
     }
 }
