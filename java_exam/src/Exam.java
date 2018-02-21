2391a41d402830098269e2c9e5dbee143155ead4
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-21 14:25:51 +0900

    Class상속, getter와 setter

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -8,24 +8,9 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-
-        //정상적으로 처리가 되면 try안에 구문이 처리되고 나눗셈이 정상적으로 수행되었다는 출력이 나옴
-      try{
-            int num1 = 10;
-            int num2 = 0;
-
-            double result = num1/num2;
-            System.out.println("결과 :"+ result);
-        }
-        //try구문이 정상처리 되지 않았을경우 에러발생 출력
-        catch(Exception e){
-          System.out.println("안내: 에러발생!");
-          e.printStackTrace();
-      }
-
-
-       System.out.println("프로그램이 정상적으로 수행되었습니다.");
-      
-
+        Man bm = new Man("홍길동");
+        System.out.print(bm.getName());
+        bm.setName("이몽룡");
+        System.out.print(bm.getName());
     }
 }
---
+++ java_exam/src/Man.java
@@ -0,0 +1,14 @@
+class Man {
+    private String name;
+    public Man(String name){
+        this.name = name;
+    }
+    //setter함수
+    public void setName(String name){
+        this.name = name;
+    }
+    //getter함수
+    public String getName(){
+        return name;
+    }
+}
