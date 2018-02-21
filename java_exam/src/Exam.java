7d9cd7b8a8ab598c68596c470fac217a6253e07a
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-02-21 10:38:58 +0900

    try~catch문 예제1

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -8,47 +8,24 @@ import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args) {
-        // frame 객체 생성
-        JFrame frame = new JFrame("title");
-        // frame 크기 설정
-        frame.setBounds(120,120,250,270);
-        // 프레임 안에 배치될 버튼들의 배치 방식 설정
-        frame.setLayout(new FlowLayout());
 
-        JCheckBox cb1 = new JCheckBox("수박");
-        JCheckBox cb2 = new JCheckBox("딸기");
-        JCheckBox cb3 = new JCheckBox("사과");
+        //정상적으로 처리가 되면 try안에 구문이 처리되고 나눗셈이 정상적으로 수행되었다는 출력이 나옴
+      try{
+            int num1 = 10;
+            int num2 = 0;
 
-        CheckBoxHandler cbHandler1 = new CheckBoxHandler(cb1);
-        CheckBoxHandler cbHandler2 = new CheckBoxHandler(cb2);
-        CheckBoxHandler cbHandler3 = new CheckBoxHandler(cb3);
-        cb1.addItemListener(cbHandler1);
-        cb2.addItemListener(cbHandler2);
-        cb3.addItemListener(cbHandler3);
-        frame.add(cb1);
-        frame.add(cb2);
-        frame.add(cb3);
+            double result = num1/num2;
+            System.out.println("결과 :"+ result);
+        }
+        //try구문이 정상처리 되지 않았을경우 에러발생 출력
+        catch(Exception e){
+          System.out.println("안내: 에러발생!");
+          e.printStackTrace();
+      }
 
-        JRadioButton rb1 = new JRadioButton("미국");
-        JRadioButton rb2 = new JRadioButton("한국");
-        JRadioButton rb3 = new JRadioButton("중국");
 
-        RadioHandler radioHandler1 = new RadioHandler(rb1);
-        RadioHandler radioHandler2 = new RadioHandler(rb2);
-        RadioHandler radioHandler3 = new RadioHandler(rb3);
-        rb1.addItemListener(radioHandler1);
-        rb2.addItemListener(radioHandler2);
-        rb3.addItemListener(radioHandler3);
+       System.out.println("프로그램이 정상적으로 수행되었습니다.");
+      
 
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(rb1);
-        bg.add(rb2);
-        bg.add(rb3);
-
-        frame.add(rb1);
-        frame.add(rb2);
-        frame.add(rb3);
-        // 프레임을 화면에 보여줌
-        frame.setVisible(true);
     }
 }
