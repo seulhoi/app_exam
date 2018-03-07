a8b19d0145fb3f77609bb18a8160f9f825cb477b
501-10-PC\501-10 <seulhoi00@gmail.com>
2018-03-07 17:37:13 +0900

    웹뷰_앱

--- Webview/app/src/main/java/com/example/a501_10/webview/MainActivity.java
+++ Webview/app/src/main/java/com/example/a501_10/webview/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.a501_10.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
         WebView webview_test;
+        EditText edittext_url;
+        ImageButton imgbtn_go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
         //2.웹뷰 컴포넌트 객체만들기
         webview_test = (WebView)findViewById(R.id.webview_test);
+        edittext_url = (EditText)findViewById(R.id.edittext_url);
+        imgbtn_go = (ImageButton)findViewById(R.id.imgbtn_go);
 
         //3.WebSetting 객체를 통해 웹뷰 설정
         WebSettings webSettings = webview_test.getSettings();
@@ -30,9 +37,21 @@ public class MainActivity extends AppCompatActivity {
                  return true;
             }
         });
-        //4.웹 페이지 로드
-        webview_test.loadUrl("http://www.nate.com");
+
+        ImgBtnListener imgBtnListener = new ImgBtnListener();
+
+        imgbtn_go.setOnClickListener(imgBtnListener);
 
 
+    }
+
+    class ImgBtnListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            String url = edittext_url.getText().toString();
+            //4.웹 페이지 로드
+            webview_test.loadUrl("http://www.nate.com");
+        }
     }
 }
